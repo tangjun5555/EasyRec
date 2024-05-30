ba6abf3dc9f5b670511084e010c0afdcea2e1672
elan.tang <1844250138@qq.com>
2024-05-30 15:38:32 +0800

    temp

--- easy_rec/python/model/rerank_model.py
+++ easy_rec/python/model/rerank_model.py
@@ -5,9 +5,8 @@ import tensorflow as tf
 
 from easy_rec.python.model.easy_rec_model import EasyRecModel
 
-
 if tf.__version__ >= '2.0':
-  tf = tf.compat.v1
+    tf = tf.compat.v1
 
 
 class ReRankModel(EasyRecModel):
@@ -20,3 +19,17 @@ class ReRankModel(EasyRecModel):
                  ):
         super(ReRankModel, self).__init__(model_config, feature_configs, features, labels, is_training)
         self._loss_type = self._model_config.loss_type
+
+        if "session_id" in features:
+            self._sample_weight = features["session_id"]
+        else:
+            self.session_ids = None
+
+    def build_predict_graph(self):
+        pass
+
+    def build_loss_graph(self):
+        pass
+
+    def get_outputs(self):
+        pass
